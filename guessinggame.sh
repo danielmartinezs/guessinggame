d17b491dcbc221ec7286d80d876ab61d1ed8676d
danielmartinezs <79072708+danielmartinezs@users.noreply.github.com>
2021-02-14 23:23:44 -0600

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,19 @@
+echo "Guessing Game"
+function guess {
+    echo "Enter the number of files that's in the current directory:"
+    read answer
+    files=$(ls -1 | wc -l)
+}
+guess
+while [[$answer -ne $files]]
+do
+    if [[$answer -lt $files]] 
+    then
+        echo "Too low. :( Please try again!"
+    else
+        echo "Too high. :( Please try again!"
+    fi
+    guess
+done
+echo "You got it!"
+echo "==" && ls -1
